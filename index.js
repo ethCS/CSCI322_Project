535376c280528d3e01437818576ef5e3e2f1fc79
ethCS <syrup199@gmail.com>
2026-02-06 09:55:05 -0700

    Fixed naming convention

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const axios = require('axios');
 const params = {};
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.MY_FIREBASE_API_KEY,
+  authDomain: process.env.MY_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.MY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.MY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.MY_FIREBASE_APP_ID,
+  measurementId: process.env.MY_FIREBASE_MEASUREMENT_ID
 };
 
 admin.initializeApp(firebaseConfig);
